b2897171a2adcc3c398c739319345380d5b210ae
[Sylwia Kwiecinska] <[sylwia.bitencourt@gmail.com]>
2022-12-12 20:07:55 +0100

    added site title

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
-import './Reset.css';
-import './App.css';
-import Header from './Components/Header'
-import Navigation from './Components/Navigation';
-import SectionEmployees from './Components/SectionEmployees';
-import SectionServices from './Components/SectionServices';
-import Footer from './Components/Footer';
-import GoogleFontLoader from 'react-google-font-loader';
-
+import "./Reset.css";
+import "./App.css";
+import Header from "./Components/Header";
+import Navigation from "./Components/Navigation";
+import SectionEmployees from "./Components/SectionEmployees";
+import SectionServices from "./Components/SectionServices";
+import Footer from "./Components/Footer";
+import GoogleFontLoader from "react-google-font-loader";
 
 function App() {
   return (
-    
     <div className="App">
       <>
-  
-    <GoogleFontLoader
-      fonts={[
-        {
-          font: 'Roboto',
-          weights: [400, '400i'],
-        },
-        {
-          font: 'Roboto Mono',
-          weights: [400, 700],
-        },
-      ]}
-      subsets={['cyrillic-ext', 'greek']}
-    />
- 
-    <p style={{ fontFamily: 'Roboto Mono, monospaced' }}>This will be in Roboto Mono!</p>
-    <p style={{ fontFamily: 'Roboto, sans-serif' }}>This will be in Roboto!</p>
-  </>
-      <Navigation/>
-      <Header/>
-      <SectionEmployees/>
-      <SectionServices/>
-      <Footer/>
+        <GoogleFontLoader
+          fonts={[
+            {
+              font: "Roboto",
+              weights: [400, "400i"],
+            },
+            {
+              font: "Roboto Mono",
+              weights: [400, 700],
+            },
+          ]}
+          subsets={["cyrillic-ext", "greek"]}
+        />
+
+        <p style={{ fontFamily: "Roboto Mono, monospaced" }}></p>
+        <p style={{ fontFamily: "Roboto, sans-serif" }}></p>
+
+        <Navigation />
+        <Header />
+        <SectionEmployees />
+        <SectionServices />
+        <Footer />
+      </>
     </div>
   );
 }
