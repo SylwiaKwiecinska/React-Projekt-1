5abc7e5039ada0cc1d0ef6f006c576dfcca4cbc2
[Sylwia Kwiecinska] <[sylwia.bitencourt@gmail.com]>
2022-12-12 09:33:58 +0100

    12.12

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -2,11 +2,11 @@ function Navigation() {
     return (
         <nav style={{backgroundColor: "#4E4E4E"}}>
             <div className="container nav-container">
-                <a className="firm">Psi raj</a>
+                <p className="firm">Psi raj</p>
                 <ul className="nav-list">
                     <li> <a href="#us" className="spn">o nas</a></li>
                     <li><a href="#offer" className="spn">oferta</a></li>
-                    <li><a href="" className="spn disabled">kontakt</a></li>
+                    <li><p className="spn disabled">kontakt</p></li>
                 </ul>
             </div>   
         </nav>
