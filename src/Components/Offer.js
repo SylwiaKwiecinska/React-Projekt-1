7b7c552669d6b2efe85c9053b4151aa734e363ba
[Sylwia Kwiecinska] <[sylwia.bitencourt@gmail.com]>
2022-12-12 19:15:53 +0100

    fontsize added to isNew

--- src/Components/Offer.js
+++ src/Components/Offer.js
@@ -4,7 +4,7 @@ function Offer({title, isNew}) {
     return (
         <div className={isNew ? "box newBox" : "box"}>
             <p className="service">{title}</p>
-            {isNew &&<p className="service">(nowość)</p>}
+            {isNew &&<p className="serviceIsNew">(nowość)</p>}
             {isNew &&<div className="orange-ellipse"></div>} 
             
         </div>
