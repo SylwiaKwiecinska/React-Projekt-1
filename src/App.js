cebcf66e411733aa722f4a29737c7068f07cb4a6
[Sylwia Kwiecinska] <[sylwia.bitencourt@gmail.com]>
2022-12-12 19:12:12 +0100

    add GoogleFontLoader

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import Navigation from './Components/Navigation';
 import SectionEmployees from './Components/SectionEmployees';
 import SectionServices from './Components/SectionServices';
 import Footer from './Components/Footer';
+import GoogleFontLoader from 'react-google-font-loader';
 
 
 function App() {
   return (
     <div className="App">
+      <GoogleFontLoader
+        fonts={[
+          {
+            font: 'Roboto',
+            weights: [400, '400i'],
+          },
+          {
+            font: 'Roboto Mono',
+            weights: [400, 700],
+          },
+        ]}
+        subsets={['cyrillic-ext', 'greek']}
+      />
+   
+      <p style={{ fontFamily: 'Roboto Mono, monospaced' }}></p>
+      <p style={{ fontFamily: 'Roboto, sans-serif' }}></p>
       <Navigation/>
       <Header/>
       <SectionEmployees/>
